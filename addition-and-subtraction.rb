03ea9f3ef0c5fce6d9f75128fdbe77d28b701f45
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2020-09-03 21:42:03 -0300

    Resoluçao dos exercicios da Aula 1 e aula 2 do curso de Ruby Puro da OneBitCode.

---
+++ addition-and-subtraction.rb
@@ -0,0 +1,11 @@
+print "Informe o primeiro número de valor inteiro: "
+firstNumber = gets.chomp.to_i
+
+print"Informe o segundo número de valor inteiro: "
+secondNumber = gets.chomp.to_i
+
+addition = firstNumber + secondNumber
+
+subtraction = firstNumber - secondNumber
+
+puts "A soma de #{firstNumber} e #{secondNumber} é #{addition} e a subtração é #{subtraction}"
---
+++ name-and-age.rb
@@ -0,0 +1,9 @@
+# Recebendo dados
+print "Por gentileza, informe seu nome completo: "
+# Guardando dados
+Fullname= gets.chomp
+
+print "Por gentileza, informe sua idade: "
+Age = gets.chomp
+
+puts "seu nome é #{Fullname} e você possui #{Age} anos."
