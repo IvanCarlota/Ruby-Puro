4dd05a00ab196cf1b3f8c93579b04b6a51e35f40
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2021-04-08 20:35:49 -0300

    Comentarios de 1 linha e de varias linhas

---
+++ Ruby-poo/comentarios-em-codigos.rb
@@ -0,0 +1,15 @@
+# comentário de uma linha
+
+puts "Acima tem um comentário de uma linha"
+
+=begin
+puts "Comentário de várias linhas"
+puts "Comentário de várias linhas"
+puts "Comentário de várias linhas"
+puts "Comentário de várias linhas"
+puts "Comentário de várias linhas"
+puts "Comentário de várias linhas"
+puts "Comentário de várias linhas"
+=end
+
+puts "Acima temos um comentário de várias linhas"
