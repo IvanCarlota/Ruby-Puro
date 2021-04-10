a910f080111cb939647a31ab30bff4e0fd24e915
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2021-04-10 20:05:40 -0300

    Usando Coerçao em Ruby, trata-se de algo similar ao casting em Java.

---
+++ Ruby-poo/04-coercao.rb
@@ -0,0 +1,10 @@
+puts "Digite a sua idade: "
+=begin
+    Para converter para inteiro, usar o "to_i"
+    Para converter para float, usar o "to_f"
+    Para converter para string, usar o "to_s"
+=end
+
+v1 = gets.chomp.to_i
+v2 = v1 + 1
+puts "Sua idade ano que vem será: #{v2}."
