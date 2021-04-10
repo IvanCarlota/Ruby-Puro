a1b79a7df356ec97d5554c6784cbe23c0d9f6310
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2021-04-10 20:38:57 -0300

    Operadores aritmeticos em Ruby

---
+++ Ruby-poo/05-operadores-aritmeticos.rb
@@ -0,0 +1,24 @@
+#adição 
+v1 = 1 + 2
+
+#subtração
+v2 = 5 - 3
+
+#multiplicação
+v3 = 4 * 2
+
+#divisão
+v4 = 4 / 2
+
+#Módulo = Resto da divisão
+v5 = 5 % 2
+
+#Potenciação
+v6 = 2 ** 3
+
+puts "Exemplo de adição: #{v1} "
+puts "Exemplo de subtração: #{v2} "
+puts "Exemplo de multiplicação: #{v3} "
+puts "Exemplo de divisão: #{v4} "
+puts "Exemplo de Módulo: #{v5} "
+puts "Exemplo de Potenciação: #{v6} "
