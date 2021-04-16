7fd547c27d75ec6e51e5daa15cb79b1b9b8c0f5b
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2021-04-15 22:13:06 -0300

    Usando a estrutura de repetiçao While e Until

---
+++ Ruby-poo/ruby-basico/10-estrutura-repeticao-While-and-until.rb
@@ -0,0 +1,15 @@
+i =1
+#Enquanto for verdadeiro será executado o trecho abaixo
+while i <= 50
+    puts "ivan - #{i}"
+    i+=1
+end
+
+#Enquanto for falso será executadoo trecho abaixo
+until i <= 0
+    puts "sou falso - #{i}"
+    i -= 1
+end
+
+#última mensagem
+puts "fim!"
